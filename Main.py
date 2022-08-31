5cbbf6840f73bc1b5445099db7ba1b977efe7523
Reuben Reny <91770238+ReubenReny03@users.noreply.github.com>
2022-08-31 11:23:06 +0530

    try_1

--- Main.py
+++ Main.py
@@ -11,22 +11,122 @@ class DoublyCircularLinkedList:
         self.count = 0
 
     def add_at_tail(self, data) -> bool:
-        # Write code here
+        if self.head is None:
+            new_node = Node(data)
+            new_node.next = new_node
+            new_node.previous = new_node
+            self.head = new_node
+            count +=1
+            return True
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            prep = self.head
+            for x in range(count-1):
+                prep = prep.next
+            prep.next = new_node
+            new_node.previous = prep
+            count +=1
+            return True
 
     def add_at_head(self, data) -> bool:
-        # Write code here
+        if self.head is None:
+            new_node = Node(data)
+            new_node.next = new_node
+            new_node.previous = new_node
+            self.head = new_node
+            count +=1
+            return True
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            prep = self.head
+            new_node.previous = prep.previous
+            prep.previous = new_node
+            count +=1
+            return True
 
     def add_at_index(self, index, data) -> bool:
-        # Write code here
-
+        if (index >= count) or (index < 0 ):
+            return False
+        if self.head is None:
+            new_node = Node(data)
+            new_node.next = new_node
+            new_node.previous = new_node
+            self.head = new_node
+            count +=1
+            return True
+        else:
+            
+            if (index == 0):
+                temp = self.head
+                new_node = Node(data)
+                new_node.next = temp
+                temp.previous = temp
+                for x in range(count - 1)
+                    temp.next
+                temp.next = new_node
+                new_node.previous = temp
+                count +=1
+                self.head = new_node
+                return True
+            else:
+                temp = self.head
+                for x in range (index)
+                    temp = temp.next
+                new_node = Node(data)
+                repre = temp.previous
+                temp.previous = new_node
+                new_node.next = temp
+                new_node.previous = repre
+                repre.next = new_node
+                count +=1
+                return True
     def get(self, index) -> int:
-        # Write code here
+        if (index >= self.count) or (index < 0):
+            return -1
+        if self.head is None:
+            return None
+        temp = self.head
+        if index == 0:
+            return temp.data
+        else:
+            for x in range (index):
+                temp = temp.next
+            return temp.data
+        
 
     def delete_at_index(self, index) -> bool:
-        # Write code here
+        if (index >= self.count) | (index < 0):
+            return False
+        if self.count == 1:
+            self.head = None
+            self.count -= 1
+            return True
+
+        target = self.head
+        for _ in range(index):
+            target = target.next
+
+        if target is self.head:
+            self.head = self.head.next
+
+        target.previous.next, target.next.previous = target.next, target.previous
+        self.count -= 1
 
     def get_previous_next(self, index) -> list:
-        # Write code here
+        if (index >= self.count) or (index < 0) or self.head is None:
+            return [-1]
+        else:
+            if self.count == 1:
+                return [self.head.previous.data, self.head.next.data]
+            else:
+                my_counter = 0
+                temp = self.head
+                while my_counter < index:
+                    temp = temp.next
+                    my_counter += 1
+                return [temp.previous.data, temp.next.data]   
 
 
 # Do not change the following code
